04efaf3383ac50f9062b9f7079cc42bc1453dae5
Daniel Gomes <76263577+dcgo15@users.noreply.github.com>
2022-06-17 16:48:57 -0300

    Add files via upload

--- dai/main.py
+++ dai/main.py
@@ -1,6 +1,30 @@
-entrada = input("Olá, eu sou a DAI , qual seu nome?: ")
+#INICIO
+#TENHO QUE CRIAR UMA NOVA CONTA NO COMMODITIES E COPIAR O CODIGO thepotato
 
-print("Seja Bem-Vindo, senhor", entrada)
-quests = input("Em que posso ajudar-lo?: ")
+print("------ DAI ------", "\nv1.0.1 - alpha")
+print("Olá, eu sou a DAI, a nova assistente do thepotato","\npara ajudar na previsão de preços : )")
+
+print("-------------------------------------")
+print("Aqui estão minhas funções:")
+
+um = "Recomendação de COMPRA/VENDA" 
+dois = "PREVISÃO DE PREÇOS"
+
+#NOME DAS FUNÇOES
+print("1-", um)
+print("2-", dois)
+
+
+while True:
+    funcao = input("Digite um ou dois para escolher a função:")
+    if funcao == "um":
+        print("Você clicou em recomendação")
+        break
+    
+    elif funcao == "dois":
+        print("Você clicou em previsão")
+        break
+    else:
+        print("Não entendi : (")
 
 #AQUI FICARÃO SUAS FUNÇÕES
