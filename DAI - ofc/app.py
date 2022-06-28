354dc3b0eaf8ff379a7c3b7b7090aa87aff02cf7
Daniel Gomes <76263577+dcgo15@users.noreply.github.com>
2022-06-28 11:12:33 -0300

    Add files via upload

--- DAI - ofc/app.py
+++ DAI - ofc/app.py
@@ -3,6 +3,12 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.lang import Builder
 from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import StringProperty,NumericProperty,BooleanProperty
+from gtts import gTTS
+import speech_recognition as sr
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import os
 
 arq_kv = '''
 <Telas>:
@@ -55,9 +61,11 @@ arq_kv = '''
         Button:
             text: "Clique aqui"
             size_hint: .2, 1
+            on_release: root.clicar()
 
         Label:
-            text: "Fale"
+            id: fale
+            text: "FALE ALGO"
 
         Label:
             text: "Maze technologies Inc"
@@ -149,7 +157,70 @@ class Telas(ScreenManager):
     pass
 
 class Inicio(Screen):
-    pass
+
+    def clicar(self):
+
+        mic = sr.Recognizer()
+
+        with sr.Microphone() as source:
+
+            mic.adjust_for_ambient_noise(source)
+
+            print("Diga alguma coisa: ")
+
+            audio = mic.listen(source)
+
+        try:
+
+            frase = mic.recognize_google(audio, language="pt-BR")
+
+            print("Você disse: ", frase)
+
+        except sr.UnkownValueError:
+            print("Não entendi")
+
+
+        #TENHO QUE USAR VOZES PARA RESPONDER
+
+        if frase == "0":
+            print("OK, MODO: MANDAR MENSAGENS ATIVADO")
+
+        elif frase == "1":
+            print("OK, MODO: MANDAR EMAIL ATIVADO")
+
+        elif frase == "pesquisar":
+            #PESQUISAR GLOBALIZAÇÃO
+            print("OK, MODO: PESQUISAR ATIVADO")
+
+            chrome_options = Options()
+            #chrome_options.add_argument("--headless")
+
+            dv = webdriver.Chrome(executable_path=r"C:\Users\WIN7\AppData\Local\Programs\Python\Python38-32\Scripts\Interfaces Graficas Py\DAI - ofc\chromedriver_win32\chromedriver.exe")
+            dv.get("https://www.google.com/")
+
+            e = driver.find_element_by_class_name("gLFyf gsfi")
+            e.send_keys("car")
+
+            button = driver.find_element_by_class_name("iblpc")
+            button.click()
+
+            
+
+        elif frase == "3":
+            print("OK, MODO: REALIZAR OPERAÇÕES ATIVADO")
+
+        elif frase == "4":
+            print("OK, MODO: FAZER TAREFAS ATIVADO")
+
+        else:
+            print("NÃO ENTENDI")
+
+
+        return frase
+
+        
+        
+   
 
 class Ajuda(Screen):
     pass
