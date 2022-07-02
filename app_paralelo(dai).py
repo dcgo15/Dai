86e54b06d55bd1fbc58163de933dc7dd4b3db501
Daniel Gomes <76263577+dcgo15@users.noreply.github.com>
2022-07-02 17:28:14 -0300

    Add files via upload

---
+++ app_paralelo(dai).py
@@ -0,0 +1,10 @@
+from kivy.config import Config, Config.set('graphics', 'resizable', True) 
+import kivy
+from kivy.app import App 
+from kivy.uix.label import Label
+
+
+class dai_teste(App): 
+    def build(self): 
+      
+dai_teste().run()
